593357406a5b883bfe53b5a580fea9162e40aaa8
Juan Martinez Alonso <juan.martineza@estudiante.uam.es>
2020-05-30 01:54:02 +0200

    v2.5 acabados los botones de redes sociales

--- js/checkbox.js
+++ js/checkbox.js
@@ -7,13 +7,15 @@ myLink.onclick = function() {
     var site = document.getElementById("website");
     if (checkBox.checked == true){
         term.style.left = "0";
+        body.style.overflow = "hidden";
         body.style.setProperty('--main-hd-color', "GhostWhite");
         // body.style.setProperty('--second-hd-color', "LightGray");
-        body.style.setProperty('--main-tit-color', "#222222");
+        body.style.setProperty('--main-tit-color', "#151515");
         // body.style.setProperty('--second-tit-color', "LightGray");
     } else {
         term.style.left = "-100%";
-        body.style.setProperty('--main-hd-color', "#222222");
+        body.style.overflow = "visible";
+        body.style.setProperty('--main-hd-color', "#151515");
         // body.style.setProperty('--second-hd-color', "LightGray");
         body.style.setProperty('--main-tit-color', "GhostWhite");
         body.style.setProperty('--second-tit-color', "LightGray");
