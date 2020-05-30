d6dd3e51ebdb77936cd4644290e5240ebca6c506
Juan Martinez Alonso <juan.martineza@estudiante.uam.es>
2020-05-30 13:59:03 +0200

    v2.6 portada lista

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+var myLink = document.getElementById('checkBox');
+var body = document.getElementsByTagName("BODY")[0];
+var intro = document.getElementById("net-nav");
+
+var opacity = 0;
+
+myLink.onclick = function() {
+    var checkBox = document.getElementById("checkBox");
+    var term = document.getElementById("terminal");
+    var site = document.getElementById("website");
+    if (checkBox.checked == true){
+        term.style.left = "0";
+        body.style.overflow = "hidden";
+        body.style.setProperty('--opacity',1);
+        body.style.setProperty('--main-hd-color', "rgba(248, 248, 255,var(--opacity))");
+        body.style.setProperty('--main-tit-color', "rgba(21, 21, 21,var(--opacity))");
+        body.style.setProperty('--main-hd-color-aw', "rgba(248, 248, 255, 1);");
+        body.style.setProperty('--main-tit-color-aw', "rgba(21, 21, 21, 1);");
+    } else {
+        term.style.left = "-100%";
+        body.style.overflow = "visible";
+        body.style.setProperty('--opacity',opacity);
+        body.style.setProperty('--main-hd-color', "rgba(21, 21, 21,var(--opacity))");
+        body.style.setProperty('--main-tit-color', "rgba(248, 248, 255,var(--opacity))");
+        body.style.setProperty('--main-hd-color-aw', "rgba(21, 21, 21, 1);");
+        body.style.setProperty('--main-tit-color-aw', "rgba(248, 248, 255, 1);");
+    }
+}
+
+window.onscroll = function(){
+    opacity = 0;
+    ypos = window.pageYOffset*2;
+    if (window.innerHeight-ypos > 0)
+        opacity = (window.innerHeight-ypos)/window.innerHeight;
+
+    opacity = 1-opacity;
+
+    body.style.setProperty('--opacity',opacity);
+
+
+
+
+
+
+};
