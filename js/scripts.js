8dec3c069ce0a0f66e85cff76facdc55b5548ab6
Juan Martinez Alonso <juan.martineza@estudiante.uam.es>
2020-05-30 16:26:21 +0200

    v2.7 arreglados errores del terminal añadidos categorias

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ var myLink = document.getElementById('checkBox');
 var body = document.getElementsByTagName("BODY")[0];
 var intro = document.getElementById("net-nav");
 
+window.onload = function(){scroll()};
+window.onscroll = function(){scroll()};
+
 var opacity = 0;
 
 myLink.onclick = function() {
     var checkBox = document.getElementById("checkBox");
+    var logo = document.getElementById("logo");
     var term = document.getElementById("terminal");
     var site = document.getElementById("website");
     if (checkBox.checked == true){
@@ -16,6 +20,7 @@ myLink.onclick = function() {
         body.style.setProperty('--main-tit-color', "rgba(21, 21, 21,var(--opacity))");
         body.style.setProperty('--main-hd-color-aw', "rgba(248, 248, 255, 1);");
         body.style.setProperty('--main-tit-color-aw', "rgba(21, 21, 21, 1);");
+        body.style.setProperty('--main-hd-color-aw', "rgba(21, 21, 21, 1);");
     } else {
         term.style.left = "-100%";
         body.style.overflow = "visible";
@@ -24,22 +29,22 @@ myLink.onclick = function() {
         body.style.setProperty('--main-tit-color', "rgba(248, 248, 255,var(--opacity))");
         body.style.setProperty('--main-hd-color-aw', "rgba(21, 21, 21, 1);");
         body.style.setProperty('--main-tit-color-aw', "rgba(248, 248, 255, 1);");
+        head.style.setProperty('-webkit-transition', ".0s;");
+        head.style.setProperty('transition', ".0s;");
     }
 }
 
-window.onscroll = function(){
+
+function scroll() {
     opacity = 0;
-    ypos = window.pageYOffset*2;
-    if (window.innerHeight-ypos > 0)
-        opacity = (window.innerHeight-ypos)/window.innerHeight;
+    ymid = (window.innerHeight-70)/2;
+    ypos = window.pageYOffset;
+
+    if (ymid > ypos)
+        opacity = 1
+    else if (ymid-ypos/2 > 0)
+        opacity = (ymid-ypos/2)/ymid;
 
     opacity = 1-opacity;
-
     body.style.setProperty('--opacity',opacity);
-
-
-
-
-
-
-};
+}
